f5b1130d04c899f17375e8b37c1a4c8dc37ef77d
Ilayda <ilayda.bakare@gmail.com>
2021-07-05 21:11:42 +0100

    Refactor and add NetworkHelper class to handle api get requests

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,8 +1,6 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import '../services/location.dart';
-import 'package:http/http.dart' as http;
+import 'package:clima/services/networking.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class LoadingScreen extends StatefulWidget {
@@ -11,41 +9,32 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  double latitude;
+  double longitude;
+
   @override
   void initState() {
     //initState only gets called once and that's when it's rendered to the screen
     super.initState();
-    getLocation();
+    getLocationData();
   }
 
   void getData() async {
-    String apiKey = dotenv.env['OPEN_WEATHER_API_KEY'];
-    http.Response response = await http.get(Uri.parse(
-        'https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=$apiKey'));
-    if (response.statusCode == 200) {
-      String data = response.body;
-
-      var decodedData = jsonDecode(
-          data); // because jsonDecode(data)'s type is dynamic we leave it as var which is for dynamic things
-      double temperature = decodedData['main']['temp'];
-      int condition = decodedData['weather'][0]['id'];
-      String cityName = decodedData['name'];
-
-      print('Temperature = $temperature');
-      print('Condition = $condition');
-      print('city = $cityName');
-    } else {
-      print(response.statusCode);
-    }
+    // because jsonDecode(data)'s type is dynamic we leave it as var which is for dynamic things
   }
 
-  void getLocation() async {
+  void getLocationData() async {
+    String apiKey = dotenv.env['OPEN_WEATHER_API_KEY'];
     Location location = Location();
+
     await location.getCurrentLocation();
-    // ⬆ we need to wait for line 20 (add await keyword) to complete before we use it, so we need to make the function we're waiting for (getCurrentLocation) a Future fn
-    print("LATITUDE: ${location.latitude}");
-    print("LONGITUDE: ${location.longitude}");
-    getData();
+    // ⬆ we need to wait for line above (add await keyword) to complete before we use it, so we need to make the function we're waiting for (getCurrentLocation) a Future fn
+    latitude = location.latitude;
+    longitude = location.longitude;
+    NetworkHelper networkHelper = NetworkHelper(
+        'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
+
+    var weatherData = await networkHelper.getData();
   }
 
   @override
@@ -56,3 +45,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
     return Scaffold();
   }
 }
+
+// double temperature = decodedData['main']['temp'];
+// int condition = decodedData['weather'][0]['id'];
+// String cityName = decodedData['name'];
